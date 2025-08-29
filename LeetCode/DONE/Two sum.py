971e13b702bf7539c0cdd308e94f19b8e1fa6057
Daria <DoshikSmirnoff@gmail.com>
2025-08-29 22:47:23 +0300

    Added solution OOP of exercise Two sum

--- LeetCode/DONE/Two sum.py
+++ LeetCode/DONE/Two sum.py
@@ -1,8 +1,6 @@
 """
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
 
 Example 1:
@@ -38,4 +36,24 @@ for i_index, i in enumerate(sorted_list):
 
 print(indexes)
 
-# Здесь надо переписать решение в виде класса и загрузить в литкод
+# OOP
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        indexes = []
+
+        sorted_list = [num for num in nums if num <= target]
+
+        for i_index, i in enumerate(sorted_list):
+            for j_index, j in enumerate(sorted_list):
+                if i_index != j_index and i + j == target:
+                    indexes = [i_index, j_index]
+                    break
+
+        return indexes
