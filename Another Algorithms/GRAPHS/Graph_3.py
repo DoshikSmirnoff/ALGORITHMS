f3b90161e860d54e53e3232b2ca8b9a2cf35c883
Daria <DoshikSmirnoff@gmail.com>
2025-09-27 00:40:56 +0300

    Added Graph_3.py

--- Another Algorithms/GRAPHS/Graph_3.py
+++ Another Algorithms/GRAPHS/Graph_3.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 graph = {
     "Misha" : ["Dima", "Sasha", "Mike"],
     "Sasha" : ["Mike", "Misha"],
@@ -5,13 +7,13 @@ graph = {
     "Dima" : ["Misha"]
 }
 
-# def all_friends(graph):
-#     print(graph["Misha"])
-#     print(graph["Sasha"])
-#     print(graph["Mike"])
-#     print(graph["Dima"])
-#
-# all_friends(graph)
+def all_friends(graph):
+    print(graph["Misha"])
+    print(graph["Sasha"])
+    print(graph["Mike"])
+    print(graph["Dima"])
+
+all_friends(graph)
 
 def dfs(graph, start, visited=None):
     if visited is None:
@@ -25,3 +27,16 @@ def dfs(graph, start, visited=None):
             dfs(graph, friend, visited)
 
 dfs(graph, "Misha", visited=None) # -> Misha Dima Sasha Mike
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start]) # queue - очередь
+
+    while queue:
+        vertex = queue.popleft() # vertex - вершина
+        if vertex not in visited:
+            print(vertex, end=" ")
+            visited.add(vertex)
+            queue.extend(graph[vertex])
+
+bfs(graph, "Misha")
